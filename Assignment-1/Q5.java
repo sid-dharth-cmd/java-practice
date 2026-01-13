6a9910d3ec46cbe47c7f3f3720300570ba245ec5
Siddharth <siddharth.23.2k5@gmail.com>
2026-01-13 13:22:15 +0530

    Updated Q5 solution

--- Assignment-1/Q5.java
+++ Assignment-1/Q5.java
@@ -1,7 +1,17 @@
-
+import java.util.Scanner;
 public class Q5 {
     public static void main(String[] args) {
-
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                sum = sum + j;
+            }
+        }
+        System.out.println("The sum of the series is: "+sum);
+        sc.close();
+     }
     
 }
